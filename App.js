6fcd29b4b21e300e93cc976598ba08512b7f9f29
jmharvey0 <jmharvey0@gmail.com>
2019-07-08 15:09:04 -0500

    Getting somewhere. Login page beginning to take shape.

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.loginContainer}>
+        <View style={styles.buttonContainer}>
+          <Button title="Create New User" color="#3a3838"/>
+        </View>
+      </View>
     </View>
   );
 }
@@ -12,8 +16,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#4e4e4e',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  loginContainer: {
+    backgroundColor: '#ffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 359,
+    width: 307,
+  },
+  buttonContainer: {
+    margin: 20,
+    paddingLeft: 20,
+    paddingRight: 20,
+    backgroundColor: '#9e9ca3',
   },
 });
